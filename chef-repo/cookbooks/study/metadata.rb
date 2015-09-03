664fec612d72b6af40402b46d9471dd01db83c7a
mjsjinsu <mjsjinsu@nate.com>
2015-09-03 17:34:52 +0900

    study cookbook add

---
+++ chef-repo/cookbooks/study/metadata.rb
@@ -0,0 +1,9 @@
+name 'study'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures study'
+long_description 'Installs/Configures study'
+version '0.1.0'
+
+depends ''
